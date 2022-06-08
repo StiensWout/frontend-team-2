fc2313df86b31d6125b2f53d56f487dc57b819fb
Wout Stiens <71498452+StiensWout@users.noreply.github.com>
2022-06-08 16:17:03 +0200

    push to update url api

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,10 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'https://my-json-server.typicode.com/StiensWout/frontend-team-2',
+        target: 'https://my-json-server.typicode.com/StiensWout/frontend-team-2/',
         changeOrigin: true,
         logLevel: 'debug',
-        pathRewrite: { '^/api': '/' },
+        //pathRewrite: { '^/api': '/' },
       },
     },
   },
